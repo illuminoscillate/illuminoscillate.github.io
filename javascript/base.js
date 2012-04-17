741bd1aca85d4abe51b1d8c7ee38dc1c16736646
Peter Fern <github@obfusc8.org>
2012-04-18 02:17:12 +1000

    Add tooltips, youtube and soundcloud feeds

---
+++ javascript/base.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+  $('a.discogs').qtip({
+    content: { text: 'Discogs' },
+    position: {
+      my: 'bottom right',
+      at: 'top center',
+    },
+    style: { classes: 'ui-tooltip-shadow ui-tooltip-tipsy' },
+  });
+
+  $('a.soundcloud').qtip({
+    content: { text: $('#soundcloudstream') },
+    position: {
+      my: 'bottom right',
+      at: 'top center',
+    },
+    hide: { event: 'mouseenter', target: $('#center, a.outlink').not('.soundcloud') },
+    style: { classes: 'ui-tooltip-shadow ui-tooltip-tipsy' },
+  });
+
+  $('a.bandcamp').qtip({
+    content: { text: 'Bandcamp' },
+    position: {
+      my: 'bottom right',
+      at: 'top center',
+    },
+    style: { classes: 'ui-tooltip-shadow ui-tooltip-tipsy' },
+  });
+
+  $('a.youtube').qtip({
+    content: { text: $('#youtubevideos') },
+    position: {
+      my: 'bottom right',
+      at: 'top center',
+    },
+    hide: { event: 'mouseenter', target: $('#center, a.outlink').not('.youtube') },
+    style: { classes: 'ui-tooltip-shadow ui-tooltip-tipsy' },
+  });
+
+  $('#youtubevideos').youTubeChannel({ 
+    userName: 'illuminoscillate', 
+    channel: "uploads", 
+    hideAuthor: true,
+    numberToDisplay: 6,
+    linksInNewWindow: true
+    //other options
+    //loadingText: "Loading...",                    
+  });
+});
